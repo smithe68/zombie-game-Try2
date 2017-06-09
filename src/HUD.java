e47b8bbcc197f17566ea9ffade001eb58d3c501d
smithe68 <Evanelectric@gmail.com>
2017-06-09 13:56:53 -0700

    Changed Main

--- src/HUD.java
+++ src/HUD.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.image.ImageObserver;
 
 /**
  * Created by evan on 6/9/2017.
@@ -10,17 +11,17 @@ public class HUD
 
     public void tick()
     {
-
+        HEALTH= Main.clamp(HEALTH,0,100);
     }
 
     public void render(Graphics g)
     {
         g.setColor(Color.gray);
-        g.fillRect(15,15,200,32);
+        g.fillRect(15,15,50,12);
         g.setColor(Color.green);
-        g.fillRect(15,15,HEALTH * 2,32);
+        g.fillRect(15,15,HEALTH/2 ,12);
         g.setColor(Color.white);
-        g.drawRect(15,15,200,32);
+        g.drawRect(15,15,50,12);
     }
 
 }
