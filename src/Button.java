66027c3422d44eb25b1646adbb394093a63553e1
Jakub <jakubshark@gmail.com>
2017-06-08 20:09:44 -0700

    More Button Work

--- src/Button.java
+++ src/Button.java
@@ -2,11 +2,13 @@ import javax.swing.*;
 
 public class Button
 {
-    public string buttonName;
-
-    public static void CreateButton(int sizeX, int sizeY)
+    public static void CreateButton(int sizeX, int sizeY, float posX, float posY)
     {
         JButton button = new JButton("Main Button");
+
+        // Set Position and Size of Button
         button.setSize(sizeX, sizeY);
+        button.setAlignmentX(posX);
+        button.setAlignmentY(posY);
     }
 }
