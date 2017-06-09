5bf964490035f1410fdbe1a97e3ae2a33c583fe2
Jakub <jakubshark@gmail.com>
2017-06-08 23:04:49 -0700

    More Button Work

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Main
+public class Main extends Canvas
 {
     public static JFrame mainScreen;
     public static JButton mainButton;
 
     public static void main(String[] args)
     {
-        JPanel mainPanel;
-
-        mainPanel = new JPanel();
+        JPanel mainPanel = new JPanel();
+        JPanel scorePanel = new JPanel();
 
         mainPanel.setSize(new Dimension(400, 300));
         mainPanel.setLayout(new BorderLayout());
--- src/Screen.java
+++ src/Screen.java
@@ -1,16 +1,20 @@
 /**
  * Created by evan on 6/8/2017.
  */
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
-public class Screen
+public class Screen extends Canvas
 {
     public static JFrame CreateScreen()
     {
         JFrame mainScreen = new JFrame();
+
         mainScreen.setSize(1920,1080);
         mainScreen.setVisible(true);
         mainScreen.setTitle("Game Window");
+        mainScreen.setResizable(false);
+        mainScreen.setLocationRelativeTo(null);
 
         return mainScreen;
     }
